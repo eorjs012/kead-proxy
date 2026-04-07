5e305e6ad86a2c0fec65880ad48787b6a3543193
eorjs012 <81949022+eorjs012@users.noreply.github.com>
2026-04-07 17:48:41 +0900

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
-                // 🔥 Authorization 제거
+                // Authorization 제거
             },
             body: JSON.stringify(req.body)
         });
