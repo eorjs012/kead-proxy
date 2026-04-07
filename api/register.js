958a2cebc5c1d37ecaf9c3d66818ec36108f8444
eorjs012 <81949022+eorjs012@users.noreply.github.com>
2026-04-07 18:09:42 +0900

    Create register.js

---
+++ api/register.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    try {
+        const response = await fetch("http://121.161.240.244:58081/api/v1/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req.body)
+        });
+
+        const data = await response.text();
+
+        res.status(200).send(data);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
