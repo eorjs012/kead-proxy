8e3690d2334b4d610a3da62d1524f9356b0209ba
eorjs012 <81949022+eorjs012@users.noreply.github.com>
2026-04-07 18:19:11 +0900

    Update register.js

--- api/register.js
+++ api/register.js
@@ -22,9 +22,9 @@ export default async function handler(req, res) {
             })
         });
 
-        const data = await response.text();
+        const data = await response.json();
 
-        res.status(200).send(data);
+        res.status(200).json(data);
 
     } catch (error) {
         res.status(500).json({ error: error.message });
